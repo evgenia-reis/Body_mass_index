07fac01cd349264457c99c335324565448e40b1e
Evgenia Reis <evgenia.kgz@gmail.com>
2023-03-01 16:12:18 +0600

    new files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double height = 1.87;
+        int weight = 98;
+        int bmi = service.calculate(height, weight);
+
+        System.out.println(bmi);
+    }
+}
